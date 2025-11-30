443c0d3b6eeef684c5fc47e010a284df1f6dafea
varadisthedev <varadisthedev@gmail.com>
2025-12-01 00:42:43 +0530

    restructure: moved frontend to client folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     //   className="bg-[image:var(--bg-image)] bg-cover bg-center min-h-screen"
     //   style={{ "--bg-image": `url(${grid})` }}
     // >
-    <div className="absolute inset-0 h-full w-full bg-yellow-50 bg-[radial-gradient(#28282866_1px,transparent_2px)] bg-[size:16px_16px]">
+    <div className="absolute inset-0 h-full w-full bg-yellow-50 bg-[radial-gradient(#28282866_1px,transparent_2px)] bg-size-[16px_16px]">
       <Navbar />
       <Placeholder />
     </div>
