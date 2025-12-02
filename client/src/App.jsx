0b03129d9a9614b30495da2c6624b338ab6a2147
varadisthedev <varadisthedev@gmail.com>
2025-12-02 23:59:19 +0530

    feat: basic draggable box

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Navbar } from "./components/Navbar";
 import grid from "./assets/grid.jpg";
 import { Placeholder } from "./components/Placeholder";
+import TestMoveable from "./components/TestMoveable";
 function App() {
   return (
     // <div
@@ -11,6 +12,7 @@ function App() {
     <div className="absolute inset-0 h-full w-full bg-yellow-50 bg-[radial-gradient(#28282866_1px,transparent_2px)] bg-size-[16px_16px]">
       <Navbar />
       <Placeholder />
+      <TestMoveable />
     </div>
   );
 }
