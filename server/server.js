e28a2d7130e9024622ae77b5e2442815168229f4
varadisthedev <varadisthedev@gmail.com>
2026-01-21 23:37:39 +0530

    add: basic express server setup

--- server/server.js
+++ server/server.js
@@ -1,12 +1,34 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const cors = require ("cors");
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+// importing routes
+import healthRoute from "./routes/healthRoute.js"
+import loginRoute from "./routes/loginRoute.js"
+dotenv.config()
+
 const app = express();
+const PORT = process.env.PORT || 8000;
 
-// middlewares
+
+// middlewares for project
 app.use(express.json());
-app.use(cors())
+app.use(express.urlencoded({ extended: true }));
 
+// addding cors 
+app.use(cors({
+    origin:true,
+    request:true
+}))
 
-// running the server
-app.listen(process.env.PORT||5000,()=>{console.log(`>>server started at: http://localhost:${process.env.PORT||5000}`)});
+app.use('/health',healthRoute);
+app.use("/login",loginRoute)
+app.use('/',(req,res)=>{
+    console.log("someone called default route");
+    res.status(200).json({message:"api is alive"});
+})
+
+// listening on a port , starting a server
+app.listen(PORT,()=>{
+    console.log(`>Server started at port: ${PORT}`)
+})
